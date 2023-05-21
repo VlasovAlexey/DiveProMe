c14f796f2f392b21da9c74f154540e70d8dd632f
VlasovAlexey <a.vlasov@gaijin.team>
2023-05-21 21:00:37 +0300

    test

--- PWA_test1/sw.js
+++ PWA_test1/sw.js
@@ -1,7 +1,6 @@
-const CACHE_NAME = 'diveprome-cache';
-
-// Add whichever assets you want to pre-cache here:
-const PRECACHE_ASSETS = [
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('divepro-me-store').then((cache) => cache.addAll([
   '/DiveProMe/PWA_test1/',
   '/DiveProMe/PWA_test1/index.html',
   '/DiveProMe/PWA_test1/index.js',
@@ -22,34 +21,13 @@ const PRECACHE_ASSETS = [
   '/DiveProMe/PWA_test1/apple-touch-icon.png',
   '/DiveProMe/PWA_test1/apple-touch-icon+.png',
   '/DiveProMe/PWA_test1/diveprome_screenshot_01.png'
-]
-
-// Listener for the install event - pre-caches our assets list on service worker install.
-self.addEventListener('install', event => {
-    event.waitUntil((async () => {
-        const cache = await caches.open(CACHE_NAME);
-        cache.addAll(PRECACHE_ASSETS);
-    })());
+    ])),
+  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(async () => {
-      const cache = await caches.open(CACHE_NAME);
-
-      // match the request to our cache
-      const cachedResponse = await cache.match(event.request);
-
-      // check if we got a valid response
-      if (cachedResponse !== undefined) {
-          // Cache hit, return the resource
-          return cachedResponse;
-      } else {
-        // Otherwise, go to the network
-          return fetch(event.request)
-      };
-  });
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
 });
