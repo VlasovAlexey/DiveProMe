b0f062b6f02a85c6ee6139318ad82ff5fa55b6f2
VlasovAlexey <a.vlasov@gaijin.team>
2023-06-04 22:53:59 +0300

    Update sw.js

--- PWA_test1/sw.js
+++ PWA_test1/sw.js
@@ -1,7 +1,12 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('divepro-me-store').then((cache) => cache.addAll([
-  '/DiveProMe/PWA_test1/',
+self.addEventListener("install", function(event) {
+  event.waitUntil(preLoad());
+});
+
+var preLoad = function(){
+  console.log("Installing web app");
+  return caches.open("diveprome-offline").then(function(cache) {
+    console.log("caching index and important routes");
+    return cache.addAll(['/DiveProMe/PWA_test1/',
   '/DiveProMe/PWA_test1/index.html',
   '/DiveProMe/PWA_test1/index.js',
   '/DiveProMe/PWA_test1/style.css',
@@ -20,14 +25,46 @@ self.addEventListener('install', (e) => {
   '/DiveProMe/PWA_test1/apple-touch-icon-512x512.png',
   '/DiveProMe/PWA_test1/apple-touch-icon.png',
   '/DiveProMe/PWA_test1/apple-touch-icon+.png',
-  '/DiveProMe/PWA_test1/diveprome_screenshot_01.png'
-    ])),
-  );
+  '/DiveProMe/PWA_test1/diveprome_screenshot_01.png']);
+  });
+};
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function(request){
+  return caches.open("diveprome-offline").then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log(response.url + " was cached");
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open("diveprome-offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match("index.html");
+     } else {
+       return matching;
+     }
+    });
+  });
+};
