cbaba470d59bb68c654a6f337af09bfe1c0136d0
VlasovAlexey <a.vlasov@gaijin.team>
2025-08-27 15:51:47 +0300

    Refactoring code
    Remove PWA support. It is not working on modern devices...
    Reorganize project structure and delete unused files.

--- Android_SRC/app/src/main/assets/index.js
+++
@@ -1,7 +0,0 @@
-// Register service worker to control making site work offline
-
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/DiveProMe/HTML_SRC/sw.js')
-    .then(() => { console.log('Service Worker Registered'); });
-}
